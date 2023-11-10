f41ef21fa235091a431ff1f612cf9fe088c185ed
Yash Yadav <yash92328@gmail.com>
2023-11-11 00:12:44 +0530

    translation service added

---
+++ core/services/translation/prompts.py
@@ -0,0 +1,11 @@
+SIMPLE_ENGLISH_TRANS_PROMPT = """
+You are working as an expert in translation. Your task is to translate the provided text to English language.
+The text that will be provided to you will be used for educational purposes. Use your intelligence to interpret the words and make properly informative sentences.
+
+Format for output: JSON
+
+{"text": <English translation of the text as a string>}
+
+Do not write anything other than the JSON object.
+
+"""
