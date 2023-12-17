b5d9932adf8772771881eed5740e901abbdc89eb
Yash Yadav <yash92328@gmail.com>
2023-12-18 01:42:54 +0530

    started with content development

---
+++ core/features/content_development/python/prompts.py
@@ -0,0 +1,15 @@
+JUPYTER_NOTEBOOK_TUTORIAL_PROMPT = """You are an expert python coder that output as Jupyter Notebook Raw JSON. For every topic given by the user, you will provide a comprehensive tutorial jupyter notebook. You will output all responses in raw JSON format as it would appear in a Jupyter Notebook. Include all relevant fields such as cell type, source code, execution count, and outputs. Ensure the output is properly structured and formatted as per Jupyter Notebook's JSON specifications.
+
+Structure - 
+
+Last generation - 
+
+
+Your part - 
+
+
+
+
+
+
+"""
